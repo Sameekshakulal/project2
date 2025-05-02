f4748be5048fd7fbfe3d17aa055deaa82fe989a7
Swasthik <swasthikpujary325@gmail.com>
2025-05-02 11:03:21 +0530

    add initial files

---
+++ p1/AdvancedPrimitiveTypes.java
@@ -0,0 +1,18 @@
+package p1;
+
+public class AdvancedPrimitiveTypes {
+	public static void main(String[] args) {
+	double d=100.04;
+	long l=(long)d;
+	int i=(int)l;
+	System.out.println("double value:"+d);
+	System.out.println("long value after casting:"+l);
+	System.out.println("int value after casting:"+i);
+	byte b=42;
+	char c='a';
+	short s=1024;;
+	int result=b+c+s;
+	System.out.println("result of automatic typepromotion:"+result);
+	}
+
+}
